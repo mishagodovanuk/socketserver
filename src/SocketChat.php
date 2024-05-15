e31c035b1f7b16e53b14b53e221fe95beb21a73b
mishagodovanuk <mishagodovanuk@gmail.com>
2024-05-15 15:40:37 +0300

    add comments

--- src/SocketChat.php
+++ src/SocketChat.php
@@ -4,14 +4,28 @@ namespace Socket\SocketChat;
 
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
+
+/**
+ * Class SocketChat
+ */
 class SocketChat implements MessageComponentInterface
 {
+    /**
+     * @var \SplObjectStorage
+     */
     protected $clients;
 
+    /**
+     * SocketChat constructor.
+     */
     public function __construct() {
         $this->clients = new \SplObjectStorage;
     }
 
+    /**
+     * @param ConnectionInterface $conn
+     * @return void
+     */
     public function onOpen(ConnectionInterface $conn) {
         // Store the new connection to send messages to later
         $this->clients->attach($conn);
@@ -19,6 +33,11 @@ class SocketChat implements MessageComponentInterface
         echo "New connection! ({$conn->resourceId})\n";
     }
 
+    /**
+     * @param ConnectionInterface $from
+     * @param $msg
+     * @return void
+     */
     public function onMessage(ConnectionInterface $from, $msg) {
         $numRecv = count($this->clients) - 1;
         echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
@@ -32,6 +51,10 @@ class SocketChat implements MessageComponentInterface
         }
     }
 
+    /**
+     * @param ConnectionInterface $conn
+     * @return void
+     */
     public function onClose(ConnectionInterface $conn) {
         // The connection is closed, remove it, as we can no longer send it messages
         $this->clients->detach($conn);
@@ -39,6 +62,11 @@ class SocketChat implements MessageComponentInterface
         echo "Connection {$conn->resourceId} has disconnected\n";
     }
 
+    /**
+     * @param ConnectionInterface $conn
+     * @param \Exception $e
+     * @return void
+     */
     public function onError(ConnectionInterface $conn, \Exception $e) {
         echo "An error has occurred: {$e->getMessage()}\n";
 
